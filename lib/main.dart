4ed632eb52fd866402aecef962474b76bbd404e4
ogash3103 <114906301+ogash3103@users.noreply.github.com>
2026-03-01 14:14:33 +0500

    done task

--- lib/main.dart
+++ lib/main.dart
@@ -1,121 +1,132 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
+class Contact {
+  final String name;
+  final String status;
+  final bool isSelected;
+
+  Contact({required this.name, required this.status, this.isSelected = false});
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+void main() {
+  runApp(MaterialApp(debugShowCheckedModeBanner: false, home: ContactsPage()));
+}
 
-  // This widget is the root of your application.
+class ContactsPage extends StatefulWidget {
+  @override
+  State<ContactsPage> createState() => _ContactsPageState();
+}
+
+class _ContactsPageState extends State<ContactsPage> {
+  List<Contact> contacts = [
+    Contact(name: 'Joe Belfiore', status: 'In a world far away'),
+    Contact(name: 'Bill Gates', status: "What I'm doing here?"),
+    Contact(name: 'Mark Zuckerberg', status: 'Really Busy, WhatsApp only'),
+    Contact(name: 'Marissa Mayer', status: 'In a rush to catch a plane'),
+    Contact(name: 'Bill Gates', status: "What I'm doing here?"),
+    Contact(name: 'Mark Zuckerberg', status: 'Really Busy, WhatsApp only'),
+    Contact(
+      name: 'Sundar Pichai',
+      status: 'Do androids dream of electric sheep?',
+    ),
+    Contact(
+      name: 'Jeff Bezos',
+      status: 'Counting Zeroes : Prime time.',
+      isSelected: true,
+    ),
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
+    return Scaffold(
+      backgroundColor: Colors.grey.shade100,
+
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 1,
+        centerTitle: true,
+        leading: Icon(Icons.menu, color: Colors.grey.shade600),
+
+        title: Text(
+          'Contacts',
+          style: TextStyle(
+            color: Colors.grey.shade700,
+            fontSize: 20,
+            fontWeight: FontWeight.w500,
+          ),
+        ),
+
+        actions: [
+          Padding(
+            padding: EdgeInsets.only(right: 16),
+            child: Icon(Icons.search, color: Colors.grey.shade600),
+          ),
+        ],
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+
+      body: ListView.builder(
+        itemCount: contacts.length,
+        itemBuilder: (context, index) {
+          return buildContactItem(contacts[index]);
+        },
+      ),
     );
   }
-}
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: .center,
-          children: [
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+  Widget buildContactItem(Contact contact) {
+    return Container(
+      color: Colors.white,
+      margin: EdgeInsets.only(bottom: 2),
+      padding: EdgeInsets.symmetric(horizontal: 20, vertical: 16),
+      child: Row(
+        children: [
+          Container(
+            width: 56,
+            height: 56,
+            decoration: BoxDecoration(
+              color: Colors.grey.shade200,
+              shape: BoxShape.circle,
             ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+          ),
+          SizedBox(width: 16),
+
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  contact.name,
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.grey.shade800,
+                  ),
+                ),
+                SizedBox(height: 4),
+
+                Text(
+                  contact.status,
+                  style: TextStyle(fontSize: 14, color: Colors.grey.shade50),
+                ),
+              ],
+            ),
+          ),
+
+          if(contact.isSelected)
+            Container(
+              width: 44,
+                height: 44,
+              decoration: BoxDecoration(
+                color: Colors.grey.shade200,
+                shape: BoxShape.circle,
+              ),
+              child: Icon(
+                Icons.edit,
+                color: Colors.grey.shade500,
+                size: 20,
+              ),
+            ),
+        ],
       ),
     );
   }
